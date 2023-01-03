bd91c3591e0686143067615ddd0cd1c47fa3f4b4
agamez <renbriggs10@gmail.com>
2023-01-02 18:11:56 -0800

    navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="navbar bg-neutral rounded-b-3xl">
+  <div class="navbar-start">
+    <p>epic button</p>
+  </div>
+  <div class="navbar-center">
+    <h1 class="text-primary normal-case text-xl">Reminder app</h1>
+  </div>
+  <div class="navbar-end">
+    <p>epic button</p>
+  </div>
+</div>
+</template>
+
+
+<script>
+export default {
+  name: "NavBar"
+}
+</script>
+
+
+<style>
+
+</style>
